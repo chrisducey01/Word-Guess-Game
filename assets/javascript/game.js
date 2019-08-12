21f072c8e00d5e65a59f7d11092126f985907610
Christopher Ducey <chrisducey01@me.com>
2019-08-11 21:01:37 -0700

    Added code in js to handle starting a new game and identifying when a user guesses a word.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,14 +5,17 @@
 const maxGuesses = 5;
 
 //globals used throughout each round
-var wordsToGuess = ["Dorothy","Sophia","Blanche","Rose"];
+var wordsToGuess = ["Dorothy", "Sophia", "Blanche", "Rose"];
 var wordsGuessed = [];  //keep track of words guessed as user successfully moves through them
 var wordToGuess;  //the word to guess for each game, pulled from the array
-var guessesRemaining = maxGuesses;
+var guessesRemaining;
 var winCount = 0;
+var gameOver = false;
+var allWordsGuessed = false;
+var currentWordGuessed = false;
 
 //globals used and reset after each word is guessed
-var instructionText = "Press any letter to start guessing";
+var instructionText;
 var lettersGuessed = [];   //keep track of letters guessed, no repeat letters
 var wrongLettersGuessed = [];   //subset of letters guessed that  are letters not in the word
 var gameBoardWordDisplay = [];  //starts by displaying the word masked with "_" for each letter
@@ -37,11 +40,11 @@ var guessedLettersObject = document.getElementById("guessedLetters");
 
   Returns:  string  (the new word to guess)
 */
-function getNewWord(){
+function getNewWord() {
     var newWord;
 
-    for(var i=0; i < wordsToGuess.length; i++){
-        if(wordsGuessed.indexOf(wordsToGuess[i]) === -1){
+    for (var i = 0; i < wordsToGuess.length; i++) {
+        if (wordsGuessed.indexOf(wordsToGuess[i]) === -1) {
             newWord = wordsToGuess[i];
             break;
         }
@@ -50,93 +53,130 @@ function getNewWord(){
     return newWord;
 }
 
-function buildMaskedWord(word){
+function buildMaskedWord(word) {
     var maskedWord = [];
-    for(var i=0; i < word.length; i++){
+    for (var i = 0; i < word.length; i++) {
         maskedWord.push("_");
     }
     return maskedWord;
 }
 
-function findLetterOccurences(array, letter){
+function findLetterOccurences(array, letter) {
     var arrayPositions = [];
     var indexPos = -1;
 
-    do{
-        if(indexPos === -1){
-            indexPos = array.toLowerCase().indexOf(letter);    
+    do {
+        if (indexPos === -1) {
+            indexPos = array.toLowerCase().indexOf(letter);
         }
-        else{
-            indexPos = array.indexOf(letter,indexPos + 1);
+        else {
+            indexPos = array.indexOf(letter, indexPos + 1);
         }
-        
-        if(indexPos > -1){
+
+        if (indexPos > -1) {
             arrayPositions.push(indexPos);
         }
-    } while(indexPos > -1)
+    } while (indexPos > -1)
     return arrayPositions;
 }
 
-function unmaskLetters(maskedWord,unmaskedWord, positions){
-    for(var i=0; i < positions.length; i++){
+function unmaskLetters(maskedWord, unmaskedWord, positions) {
+    for (var i = 0; i < positions.length; i++) {
         var pos = positions[i];
         maskedWord[pos] = unmaskedWord[pos];
     }
     return maskedWord;
 }
 
+function initGameTurn() {
+    wordToGuess = getNewWord();
+    currentWordGuessed = false;
+    console.log(wordToGuess);
+    console.log(wordToGuess.length);
+    if (wordToGuess.length > 0) {
+        gameBoardWordDisplay = buildMaskedWord(wordToGuess);
+        console.log(gameBoardWordDisplay);
+        lettersObject.textContent = gameBoardWordDisplay.join(' ');
+        instructionText = "Press any letter to start guessing";
+        instructionTextObject.textContent = instructionText;
+        guessesRemaining = maxGuesses;
+        guessesObject.textContent = "Guesses Remaining: " + guessesRemaining;    
+        lettersGuessed = [];
+        guessedLettersObject.textContent = lettersGuessed.join(' ');
+    }
+    else {
+        allWordsGuessed = true;
+        gameOver = true;
+    }
 
-
-
-document.onload = function(event){
-    instructionTextObject.textContent = instructionText;
-    lettersObject.textContent = gameBoardWordDisplay;
-    guessesObject.textContent = "Guesses Remaining: " + guessesRemaining;
 }
 
-document.onkeyup = function(event){
+function initNewGame(){
+    winCount = 0;
+    allWordsGuessed = false;
+    currentWordGuessed = false;
+    gameOver = false;
+    wordsGuessed = [];
+    initGameTurn();
+}
+
+
+document.onkeyup = function (event) {
     letterGuessed = event.key.toLowerCase();
 
-    //Check to make sure the letter hasn't been guessed yet before playing it
-    if(lettersGuessed.indexOf(letterGuessed) === -1){
-        lettersGuessed.push(letterGuessed);
-
-        console.log("Before guess: " + gameBoardWordDisplay);
-        var positions = findLetterOccurences(wordToGuess, letterGuessed);
-
-        //GUESSED WRONG!
-        //If the letter wasn't found at all in the word  to guess, tell them to try again
-        //Decrease the amount of guesses remaining
-        if(positions.length < 1){
-            instructionText = "Sorry, the letter '" + letterGuessed + "' isn't in the word.  Try again.";
-            instructionTextObject.textContent = instructionText;
-            guessesRemaining--;
-            guessesObject.textContent = "Guesses Remaining: " + guessesRemaining;
-            wrongLettersGuessed.push(letterGuessed);
-            console.log("Letter Guessed: " + letterGuessed);
-            console.log("Wrong Letter Array: " + wrongLettersGuessed);
-            guessedLettersObject.textContent = wrongLettersGuessed.join(' ');
-        }
-        //GUESSED RIGHT!
-        //Otherwise it was found, unmask the letter every place it was found in the word
-        else{
-            console.log("Found the letter here: " + positions);
-            instructionText = "Nice job!  The letter '" + letterGuessed + "' is in the word.  Guess again.";
-            instructionTextObject.textContent = instructionText;
-            gameBoardWordDisplay = unmaskLetters(gameBoardWordDisplay, wordToGuess, positions);
-            lettersObject.textContent = gameBoardWordDisplay.join(' ');
-        }
-
+    if (currentWordGuessed) {
+        initGameTurn();
     }
-    //Otherwise make them guess again
-    else{
-        instructionText = "You've already guessed the letter '" + letterGuessed + "'.  Try another one.";
-        instructionTextObject.textContent = instructionText;
+    else if (gameOver) {
+        initNewGame();
     }
+    else {
 
+        //Check to make sure the letter hasn't been guessed yet before playing it
+        if (lettersGuessed.indexOf(letterGuessed) === -1) {
+            lettersGuessed.push(letterGuessed);
+            var positions = findLetterOccurences(wordToGuess, letterGuessed);
 
-    if(gameBoardWordDisplay.indexOf("_") == -1){
+            //GUESSED WRONG!
+            //If the letter wasn't found at all in the word  to guess, tell them to try again
+            //Decrease the amount of guesses remaining
+            if (positions.length < 1) {
+                instructionText = "Sorry, the letter '" + letterGuessed + "' isn't in the word.";
+                instructionTextObject.textContent = instructionText;
+                guessesRemaining--;
+                guessesObject.textContent = "Guesses Remaining: " + guessesRemaining;
+                wrongLettersGuessed.push(letterGuessed);
+                guessedLettersObject.textContent = wrongLettersGuessed.join(' ');
 
+                //Check if they used up all their guesses
+                //NOTE:  Still need to add in text to update the words on the screen so they know they lost
+                if(guessesRemaining < 1){
+                    gameOver = true;
+                }
+            }
+            //GUESSED RIGHT!
+            //Letter was found, unmask the letter every place it was found in the word
+            else {
+                instructionText = "Nice job!  The letter '" + letterGuessed + "' is in the word.";
+                instructionTextObject.textContent = instructionText;
+                gameBoardWordDisplay = unmaskLetters(gameBoardWordDisplay, wordToGuess, positions);
+                lettersObject.textContent = gameBoardWordDisplay.join(' ');
+
+                //If the word is guessed, they won that round
+                if (gameBoardWordDisplay.indexOf("_") === -1) {
+                    currentWordGuessed = true;
+                    winCount++;
+                    instructionText = "Nice job you guessed the word!  Press any key to guess a new word.";
+                    instructionTextObject.textContent = instructionText;
+                }
+            }
+
+        }
+        //Otherwise make them guess again
+        else {
+            instructionText = "You've already guessed the letter '" + letterGuessed + "'.  Try another one.";
+            instructionTextObject.textContent = instructionText;
+        }
     }
 };
 
@@ -145,9 +185,4 @@ document.onkeyup = function(event){
    MAIN LOGIC GOES BELOW
 */
 
-wordToGuess = getNewWord();
-if(wordToGuess.length > 0){
-    gameBoardWordDisplay = buildMaskedWord(wordToGuess);
-    lettersObject.textContent = gameBoardWordDisplay.join(' ');
-    console.log(gameBoardWordDisplay);
-}
+initNewGame();
