c4b372c9804a1de3ad294e3da28394ad90f5cb2f
Christopher Ducey <chrisducey01@me.com>
2019-08-16 17:20:39 -0700

    Updated layout to fit full viewport height and center container

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,16 @@
 */
 const maxGuesses = 5;
 const wordArray = [
-    {wordToGuess: "Ghostbusters", imgSrc: "assets/images/ghostbusters.jpg"},
-    {wordToGuess: "Stripes", imgSrc: "assets/images/stripes.jpg"},
-    {wordToGuess: "Groundhog Day", imgSrc: "assets/images/groundhog_day.jpg"},
-    {wordToGuess: "CaddyShack", imgSrc: "assets/images/caddyshack.jpg"},
-    {wordToGuess: "Lost in Translation", imgSrc: "assets/images/lost_in_translation.jpg"},
-    {wordToGuess: "Scrooged", imgSrc: "assets/images/scrooged.jpg"},
-    {wordToGuess: "Kingpin", imgSrc: "assets/images/kingpin.jpg"},
-    {wordToGuess: "What About Bob", imgSrc: "assets/images/what_about_bob.jpg"},
-    {wordToGuess: "ZombieLand", imgSrc: "assets/images/zombieland.jpg"},
-    {wordToGuess: "Meatballs", imgSrc: "assets/images/meatballs.jpg"}
+    { wordToGuess: "Ghostbusters", imgSrc: "assets/images/ghostbusters.jpg" },
+    { wordToGuess: "Stripes", imgSrc: "assets/images/stripes.jpg" },
+    { wordToGuess: "Groundhog Day", imgSrc: "assets/images/groundhog_day.jpg" },
+    { wordToGuess: "CaddyShack", imgSrc: "assets/images/caddyshack.jpg" },
+    { wordToGuess: "Lost in Translation", imgSrc: "assets/images/lost_in_translation.jpg" },
+    { wordToGuess: "Scrooged", imgSrc: "assets/images/scrooged.jpg" },
+    { wordToGuess: "Kingpin", imgSrc: "assets/images/kingpin.jpg" },
+    { wordToGuess: "What About Bob", imgSrc: "assets/images/what_about_bob.jpg" },
+    { wordToGuess: "ZombieLand", imgSrc: "assets/images/zombieland.jpg" },
+    { wordToGuess: "Meatballs", imgSrc: "assets/images/meatballs.jpg" }
 ];
 
 const guessImgPath = "assets/images/question_mark.png";
@@ -29,43 +29,50 @@ var scoreObject = document.getElementById("score");
 var imageObject = document.getElementById("answerImage");
 
 var gameObject = {
-    wordsToGuessArray : wordArray.slice(),
-    wordsGuessed : [],
-    wordArrayPos : -1,
+    wordsToGuessArray: wordArray.slice(),
+    wordsGuessed: [],
+    wordArrayPos: -1,
 
-    wordToGuess : "",
-    guessesRemaining : 0,
-    winCount : 0,
-    gameOver : false,
-    allWordsGuessed : false,
-    currentWordGuessed : false,
+    wordToGuess: "",
+    guessesRemaining: 0,
+    winCount: 0,
+    gameOver: false,
+    allWordsGuessed: false,
+    currentWordGuessed: false,
 
-    instructionText : "",
-    lettersGuessed : [],        //keep track of letters guessed, no repeat letters
-    wrongLettersGuessed : [],   //subset of letters guessed that  are letters not in the word
-    gameBoardWordDisplay : [],  //starts by displaying the word masked with "_" for each letter
-    letterGuessed : "",
+    instructionText: "",
+    lettersGuessed: [],        //keep track of letters guessed, no repeat letters
+    wrongLettersGuessed: [],   //subset of letters guessed that  are letters not in the word
+    gameBoardWordDisplay: [],  //starts by displaying the word masked with "_" for each letter
+    letterGuessed: "",
 
 
-    getNewWord : function() {
+    /*
+      Name:  getNewWord
+
+      Purpose:  Pulls a new word out of the master word array.  Makes
+        sure the word isn't one that has already been guessed.
+
+      Returns:  string  (the new word to guess) 
+    */
+    getNewWord: function () {
         var newWord;
-        
 
-        if(this.wordsToGuessArray.length < 1){
+
+        if (this.wordsToGuessArray.length < 1) {
             newWord = "";
             this.wordArrayPos = -1;
         }
-        else
-        {
+        else {
             var randomNum = Math.floor(Math.random() * this.wordsToGuessArray.length);
             newWord = this.wordsToGuessArray[randomNum].wordToGuess;
             this.wordArrayPos = randomNum;
         }
-    
+
         return newWord;
     },
 
-    buildMaskedWord : function(word) {
+    buildMaskedWord: function (word) {
         var maskedWord = [];
         for (var i = 0; i < word.length; i++) {
             if (word[i] === " ") {
@@ -78,7 +85,7 @@ var gameObject = {
         return maskedWord;
     },
 
-    unmaskLetters : function(maskedWord, unmaskedWord, positions) {
+    unmaskLetters: function (maskedWord, unmaskedWord, positions) {
         for (var i = 0; i < positions.length; i++) {
             var pos = positions[i];
             maskedWord[pos] = unmaskedWord[pos];
@@ -86,7 +93,7 @@ var gameObject = {
         return maskedWord;
     },
 
-    initNewGame : function() {
+    initNewGame: function () {
         this.winCount = 0;
         scoreObject.textContent = this.winCount;
         this.allWordsGuessed = false;
@@ -94,20 +101,17 @@ var gameObject = {
         this.gameOver = false;
         this.wordsGuessed = [];
         this.wordsToGuessArray = wordArray.slice();
-        console.log(this.wordsToGuessArray);
-        console.log(wordArray);
         this.initGameTurn();
     },
 
-    initGameTurn : function() {
+    initGameTurn: function () {
         this.wordToGuess = this.getNewWord();
-        console.log("initGameTurn: word to guess is: " + this.wordToGuess);
         this.currentWordGuessed = false;
         this.lettersGuessed = [];
         this.wrongLettersGuessed = [];
-    
+
         if (this.wordToGuess.length > 0) {
-            imageObject.setAttribute("src",guessImgPath);
+            imageObject.setAttribute("src", guessImgPath);
             this.gameBoardWordDisplay = this.buildMaskedWord(this.wordToGuess);
             lettersObject.textContent = this.gameBoardWordDisplay.join(' ');
             this.instructionText = "Press any letter to start guessing";
@@ -117,13 +121,13 @@ var gameObject = {
             this.lettersGuessed = [];
             guessedLettersObject.textContent = this.lettersGuessed.join(' ');
         }
-    
-    },    
-    
-    findLetterOccurences : function(array, letter) {
+
+    },
+
+    findLetterOccurences: function (array, letter) {
         var arrayPositions = [];
         var indexPos = -1;
-    
+
         do {
             if (indexPos === -1) {
                 indexPos = array.toLowerCase().indexOf(letter);
@@ -131,7 +135,7 @@ var gameObject = {
             else {
                 indexPos = array.toLowerCase().indexOf(letter, indexPos + 1);
             }
-    
+
             if (indexPos > -1) {
                 arrayPositions.push(indexPos);
             }
@@ -139,42 +143,37 @@ var gameObject = {
         return arrayPositions;
     },
 
-    moveGuessedWord : function(){
-        this.wordsGuessed.push(this.wordsToGuessArray.splice(this.wordArrayPos,1));
+    moveGuessedWord: function () {
+        this.wordsGuessed.push(this.wordsToGuessArray.splice(this.wordArrayPos, 1));
     }
-    
+
 };
 
+
 /*
-  Name:  getNewWord
-
-  Purpose:  Pulls a new word out of the master word array.  Makes
-    sure the word isn't one that has already been guessed.
-
-  Returns:  string  (the new word to guess)
+  Most of the logic is invoked based on each time a user presses a key
 */
-
 document.onkeyup = function (event) {
     gameObject.letterGuessed = event.key.toLowerCase();
 
+    //If the user guessed the word but the game isn't over, start a new game turn
     if (gameObject.currentWordGuessed && !gameObject.gameOver) {
         gameObject.initGameTurn();
 
         //If no words are returned, the game is over and the user guessed all the words
-        if(gameObject.wordToGuess.length == 0){
-            console.log("Game is over you guessed all the words");
+        if (gameObject.wordToGuess.length == 0) {
             gameObject.instructionText = "Congratulations!  You guessed all of the words!  Huzzah!  Press any key to begin a new game.";
-            instructionTextObject.textContent =  gameObject.instructionText;
+            instructionTextObject.textContent = gameObject.instructionText;
             gameObject.gameOver = true;
             gameObject.allWordsGuessed = true;
         }
     }
+    //If the game is over, start a new game
     else if (gameObject.gameOver) {
-        console.log("Game is over");
         gameObject.initNewGame();
-        console.log("stuff");
     }
-    else if( (gameObject.letterGuessed.length == 1) && (gameObject.letterGuessed.search(/[a-z]/g) >= 0) ){
+    //If the user is still guessing a word, only count letters of the alphabet (ignore numbers, function keys, etc)
+    else if ((gameObject.letterGuessed.length == 1) && (gameObject.letterGuessed.search(/[a-z]/g) >= 0)) {
 
         //Check to make sure the letter hasn't been guessed yet before playing it
         if (gameObject.lettersGuessed.indexOf(gameObject.letterGuessed) === -1) {
@@ -193,7 +192,6 @@ document.onkeyup = function (event) {
                 guessedLettersObject.textContent = gameObject.wrongLettersGuessed.join(' ');
 
                 //Check if they used up all their guesses
-                //NOTE:  Still need to add in text to update the words on the screen so they know they lost
                 if (gameObject.guessesRemaining < 1) {
                     gameObject.gameOver = true;
                     gameObject.instructionText = "Well shucky darn you used up all your guesses.  Looks like you lose the game.  Press any key to start a new game.";
@@ -210,19 +208,20 @@ document.onkeyup = function (event) {
                 lettersObject.textContent = gameObject.gameBoardWordDisplay.join(' ');
 
                 //If the word is guessed, they won that round
+                //Set currentWordGuessed to true so a new turn can start
                 if (gameObject.gameBoardWordDisplay.indexOf("_") === -1) {
                     gameObject.currentWordGuessed = true;
                     gameObject.winCount++;
                     scoreObject.textContent = gameObject.winCount;
                     gameObject.instructionText = "Nice job you guessed the word!  Press any key to guess a new word.";
                     instructionTextObject.textContent = gameObject.instructionText;
-                    imageObject.setAttribute("src",gameObject.wordsToGuessArray[gameObject.wordArrayPos].imgSrc);
+                    imageObject.setAttribute("src", gameObject.wordsToGuessArray[gameObject.wordArrayPos].imgSrc);
                     gameObject.moveGuessedWord();
                 }
             }
 
         }
-        //Otherwise make them guess again
+        //Otherwise the user already selected this letter, ignore the input and make them guess again
         else {
             gameObject.instructionText = "You've already guessed the letter '" + gameObject.letterGuessed + "'.  Try another one.";
             instructionTextObject.textContent = gameObject.instructionText;
@@ -230,9 +229,7 @@ document.onkeyup = function (event) {
     }
 };
 
-
 /*
-   MAIN LOGIC GOES BELOW
+  Initialize a new game when the page is loaded
 */
-
 gameObject.initNewGame();
