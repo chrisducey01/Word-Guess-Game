c7004c01147d86511f68c1704766af43223857e8
Christopher Ducey <chrisducey01@me.com>
2019-08-17 21:21:21 -0700

    Multiple words now show on separate lines.  Added sound effects for win/loss.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 /*
    GLOBAL VARIABLES
 */
-const maxGuesses = 5;
+const maxGuesses = 8;
 const wordArray = [
     { wordToGuess: "Ghostbusters", imgSrc: "assets/images/ghostbusters.jpg" },
     { wordToGuess: "Stripes", imgSrc: "assets/images/stripes.jpg" },
@@ -12,13 +12,12 @@ const wordArray = [
     { wordToGuess: "Scrooged", imgSrc: "assets/images/scrooged.jpg" },
     { wordToGuess: "Kingpin", imgSrc: "assets/images/kingpin.jpg" },
     { wordToGuess: "What About Bob", imgSrc: "assets/images/what_about_bob.jpg" },
-    { wordToGuess: "ZombieLand", imgSrc: "assets/images/zombieland.jpg" },
+    { wordToGuess: "Zombieland", imgSrc: "assets/images/zombieland.jpg" },
     { wordToGuess: "Meatballs", imgSrc: "assets/images/meatballs.jpg" }
 ];
 
 const guessImgPath = "assets/images/question_mark.png";
 const loseImgPath = "assets/images/game_over.png";
-
 
 //grab elements from the html by id
 var instructionTextObject = document.getElementById("instructions");
@@ -39,13 +38,56 @@ var gameObject = {
     gameOver: false,
     allWordsGuessed: false,
     currentWordGuessed: false,
+    loseSoundFile : "assets/sounds/sadtrombone.wav",
+    winSoundFile : "assets/sounds/yay.wav",
 
     instructionText: "",
     lettersGuessed: [],        //keep track of letters guessed, no repeat letters
     wrongLettersGuessed: [],   //subset of letters guessed that  are letters not in the word
     gameBoardWordDisplay: [],  //starts by displaying the word masked with "_" for each letter
+    gameBoardHtmlObjects: [],  //an h2 object for each word in the gameBoardWordDisplay array
     letterGuessed: "",
 
+    initNewGame: function () {
+        this.winCount = 0;
+        scoreObject.textContent = this.winCount;
+        this.allWordsGuessed = false;
+        this.currentWordGuessed = false;
+        this.gameOver = false;
+        this.wordsGuessed = [];
+        this.wordsToGuessArray = wordArray.slice();
+        this.initGameTurn();
+    },
+
+    initGameTurn: function () {
+        this.wordToGuess = this.getNewWord();
+        this.currentWordGuessed = false;
+        this.lettersGuessed = [];
+        this.wrongLettersGuessed = [];
+
+        if (this.wordToGuess.length > 0) {
+            imageObject.setAttribute("src", guessImgPath);
+            this.gameBoardWordDisplay = this.buildMaskedWord(this.wordToGuess);
+            
+            //empty the div if there is anything in there from the previous turn
+            while (lettersObject.hasChildNodes()) {
+                lettersObject.removeChild(lettersObject.firstChild);
+            }
+
+            this.gameBoardHtmlObjects = this.getMaskedHtml(this.gameBoardWordDisplay);
+            for(var i=0; i < this.gameBoardHtmlObjects.length; i++){
+                lettersObject.appendChild(this.gameBoardHtmlObjects[i]);
+            }
+
+            this.instructionText = "Press any letter to start guessing";
+            instructionTextObject.textContent = this.instructionText;
+            this.guessesRemaining = maxGuesses;
+            guessesObject.textContent = "Guesses Remaining: " + this.guessesRemaining;
+            this.lettersGuessed = [];
+            guessedLettersObject.textContent = this.lettersGuessed.join(' ');
+        }
+
+    },
 
     /*
       Name:  getNewWord
@@ -74,6 +116,7 @@ var gameObject = {
 
     buildMaskedWord: function (word) {
         var maskedWord = [];
+
         for (var i = 0; i < word.length; i++) {
             if (word[i] === " ") {
                 maskedWord.push("|");
@@ -82,46 +125,21 @@ var gameObject = {
                 maskedWord.push("_");
             }
         }
+
         return maskedWord;
     },
 
-    unmaskLetters: function (maskedWord, unmaskedWord, positions) {
-        for (var i = 0; i < positions.length; i++) {
-            var pos = positions[i];
-            maskedWord[pos] = unmaskedWord[pos];
-        }
-        return maskedWord;
-    },
+    getMaskedHtml(words){
+        var htmlObjects = [];
+        var wordsArray = words.join(' ').split('|');
 
-    initNewGame: function () {
-        this.winCount = 0;
-        scoreObject.textContent = this.winCount;
-        this.allWordsGuessed = false;
-        this.currentWordGuessed = false;
-        this.gameOver = false;
-        this.wordsGuessed = [];
-        this.wordsToGuessArray = wordArray.slice();
-        this.initGameTurn();
-    },
-
-    initGameTurn: function () {
-        this.wordToGuess = this.getNewWord();
-        this.currentWordGuessed = false;
-        this.lettersGuessed = [];
-        this.wrongLettersGuessed = [];
-
-        if (this.wordToGuess.length > 0) {
-            imageObject.setAttribute("src", guessImgPath);
-            this.gameBoardWordDisplay = this.buildMaskedWord(this.wordToGuess);
-            lettersObject.textContent = this.gameBoardWordDisplay.join(' ');
-            this.instructionText = "Press any letter to start guessing";
-            instructionTextObject.textContent = this.instructionText;
-            this.guessesRemaining = maxGuesses;
-            guessesObject.textContent = "Guesses Remaining: " + this.guessesRemaining;
-            this.lettersGuessed = [];
-            guessedLettersObject.textContent = this.lettersGuessed.join(' ');
+        for(var i=0; i < wordsArray.length; i++){
+            var h2Object = document.createElement("h2");
+            h2Object.textContent = wordsArray[i];
+            htmlObjects.push(h2Object);
         }
 
+        return htmlObjects;
     },
 
     findLetterOccurences: function (array, letter) {
@@ -141,6 +159,14 @@ var gameObject = {
             }
         } while (indexPos > -1)
         return arrayPositions;
+    },
+
+    unmaskLetters: function (maskedWord, unmaskedWord, positions) {
+        for (var i = 0; i < positions.length; i++) {
+            var pos = positions[i];
+            maskedWord[pos] = unmaskedWord[pos];
+        }
+        return maskedWord;
     },
 
     moveGuessedWord: function () {
@@ -197,6 +223,7 @@ document.onkeyup = function (event) {
                     gameObject.instructionText = "Well shucky darn you used up all your guesses.  Looks like you lose the game.  Press any key to start a new game.";
                     instructionTextObject.textContent = gameObject.instructionText;
                     imageObject.setAttribute("src", loseImgPath);
+                    new Audio(gameObject.loseSoundFile).play();
                 }
             }
             //GUESSED RIGHT!
@@ -205,7 +232,13 @@ document.onkeyup = function (event) {
                 gameObject.instructionText = "Nice job!  The letter '" + gameObject.letterGuessed + "' is in the word.";
                 instructionTextObject.textContent = gameObject.instructionText;
                 gameObject.gameBoardWordDisplay = gameObject.unmaskLetters(gameObject.gameBoardWordDisplay, gameObject.wordToGuess, positions);
-                lettersObject.textContent = gameObject.gameBoardWordDisplay.join(' ');
+                gameObject.gameBoardHtmlObjects = gameObject.getMaskedHtml(gameObject.gameBoardWordDisplay);
+                var children = lettersObject.children;
+                for(var i=0; i < gameObject.gameBoardHtmlObjects.length; i++){
+                    if(i < children.length){
+                        children[i].textContent = gameObject.gameBoardHtmlObjects[i].textContent;
+                    }
+                }                
 
                 //If the word is guessed, they won that round
                 //Set currentWordGuessed to true so a new turn can start
@@ -216,6 +249,7 @@ document.onkeyup = function (event) {
                     gameObject.instructionText = "Nice job you guessed the word!  Press any key to guess a new word.";
                     instructionTextObject.textContent = gameObject.instructionText;
                     imageObject.setAttribute("src", gameObject.wordsToGuessArray[gameObject.wordArrayPos].imgSrc);
+                    new Audio(gameObject.winSoundFile).play();
                     gameObject.moveGuessedWord();
                 }
             }
